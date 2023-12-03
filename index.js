6fd604dceb76292d188e632f935fa600cbc8610b
yerman2 <yermanratia2@gmail.com>
2023-12-02 23:53:40 -0400

    cambios liegeros

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+// Importar el módulo dotenv e inicializar las variables de entorno desde el archivo .env
 require("dotenv").config();
 
 const http = require("http");
 
 function requestController() {
     console.log('Console Log Funcion con la Logica');
-
 }
 
 // Configurar el servidor
 const server = http.createServer(requestController);
 
-const PORT = process.env.PORT;
+// Obtener el puerto de las variables de entorno o utilizar el puerto 4000 por defecto
+const PORT = process.env.PORT || 4000;
 
-server.listen(400, function () {
+server.listen(PORT, function () {
     console.log(`El servidor está escuchando en el puerto ${PORT}`);
-})
+});
