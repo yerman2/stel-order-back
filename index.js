9da5eda81295c3e718d5cb42a00e084aed5b9a51
yerman2 <yermanratia2@gmail.com>
2023-12-02 23:03:31 -0400

    Cambios para el Deploy

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola!!!");
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`El servidor está escuchando en el puerto ${port}`);
+
+  res.send('¡Hola, Deploy test!');
+});
