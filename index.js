219f54b7404a92eb92e235d2d220e7bec694ba1e
yerman2 <yermanratia2@gmail.com>
2023-12-04 21:41:17 -0400

    restaurar 0

--- index.js
+++ index.js
@@ -89,4 +89,5 @@ async function requestController(req, res) {
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ message: "Peticiones realizadas correctamente" }));
 }
-// Coded by yerman2
+
+// Coded by yerman2 
